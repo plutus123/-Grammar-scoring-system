e52c3ba57bd2ce9695766e3aa9ae770a8f4a1838
Plutus <128125148+plutus123@users.noreply.github.com>
2025-04-07 01:48:03 +0530

    Delete grammar_scorer directory

--- grammar_scorer/__init__.py
+++
@@ -1,8 +0,0 @@
-# Package initialization
-# Import and expose key components
-
-from .grammar_analyzer import GrammarAnalyzer
-from .audio_processor import AudioProcessor
-from .scoring import GrammarScorer
-
-__all__ = ['GrammarAnalyzer', 'AudioProcessor', 'GrammarScorer']
